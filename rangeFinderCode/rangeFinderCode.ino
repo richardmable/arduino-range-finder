fe273ae54b145dc6b9057e9784f4fbf35f6a5e37
Richard Mable <richardbmable@gmail.com>
2016-04-10 14:13:55 -0400

    added a readme and basic program outline to display distance

--- rangeFinderCode/rangeFinderCode.ino
+++ rangeFinderCode/rangeFinderCode.ino
@@ -2,14 +2,33 @@
 
 // this names the RangeFinder as proximity
 EYW::RangeFinder proximity;
+// declare an integer var named distance and set to zero
+// do before setup() becuase it will be used throughout the program
+// this is a global var in C/C++
+int distance = 0;
+
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // this is the setup code which will run once
+  Serial.begin(9600);
+  // this calibrates the sensor with an object 10cm away
+  // it will then sound an alarm when it has been calibrated
+  proximity.begin();
+  proximity.alarm();
+  proximity.calibrate(10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // this is the code that will run over and over again
+  // here it will take a distance measurement, set it equal to distance var
+  // then it will print the distance
+  distance = proximity.getDistance();
+  Serial.print ("Current Distance: ");
+  Serial.println(distance);
 
+  if (distance < 10)
+  {
+    proximity.alarm(2,2000,100);
+  }
 }
---
+++ readme.md
@@ -0,0 +1,1 @@
+#Code for Adruino board to use and print data from the rangefinder module
