9b270b4a2b5cac9c275eb4354d29e67babda9876
Richard Mable <richardbmable@gmail.com>
2016-04-10 14:02:15 -0400

    init commit

---
+++ rangeFinderCode/rangeFinderCode.ino
@@ -0,0 +1,15 @@
+#include <EYW.h>
+
+// this names the RangeFinder as proximity
+EYW::RangeFinder proximity;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
